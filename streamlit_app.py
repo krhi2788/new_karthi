7ae15708566252daf8933668d990b77d6d01ace0
krhi2788 <113953169+krhi2788@users.noreply.github.com>
2022-09-19 21:05:01 -0400

    Update streamlit_app.py
    breakfast menu changfed to favourites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parent New healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favorites')
 streamlit.text('Some Apples and Oranges')
 streamlit.text('A cup of tea')
 streamlit.text('White bread')
