5cce522fa4b7c38edee97d65213b8c1367b2fd4a
krhi2788 <113953169+krhi2788@users.noreply.github.com>
2022-09-19 21:06:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parent New healthy Dinner')
+streamlit.title(' :) My Parent New healthy Dinner')
 streamlit.header('Breakfast favorites')
 streamlit.text('Some Apples and Oranges')
 streamlit.text('A cup of tea')
