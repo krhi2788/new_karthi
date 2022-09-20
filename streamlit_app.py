3967bcc0ac77a6469268638041a83ebc664da178
krhi2788 <113953169+krhi2788@users.noreply.github.com>
2022-09-19 21:02:39 -0400

    Update streamlit_app.py
    New breakfast mnenu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parent New healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Some Apples and Oranges')
+streamlit.text('A cup of tea')
+streamlist.text('White bread')
